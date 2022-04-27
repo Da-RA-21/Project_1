b657da5110ed14f9a41b09bbeeb5e0eea96de9b9
Da_RA_21 <rashawnthompson@unomaha.edu>
2022-04-27 03:48:34 -0500

    controller.py and areaInput.py created to handle GUI functionality of code. Next push should contain code that will display result of calculations in the GUI application after input is given in appropriate boxes

---
+++ controller.py
@@ -0,0 +1,4 @@
+from PyQt5.QtWidgets import *
+import areaInput
+# TODO: Make sure the GUI turns all values into floats so your type hinting is correct
+# TODO: Make sure in working with values that use pi that you limit significant figures displayed in the answer
