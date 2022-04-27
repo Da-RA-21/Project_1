088f9df1eb40e9b45a0f8488e5c3e8e0a7e33422
Da_RA_21 <rashawnthompson@unomaha.edu>
2022-04-26 19:05:23 -0500

    For rashawn_thompson.py: docstrings and type hinting added and code modified to handle boolean types being passed as arguments For test_rashawn_thompson.py: appropriate tests added for code modifications in rashawn_thompson.py

--- rashawn_thompson.py
+++ rashawn_thompson.py
@@ -1,52 +1,76 @@
 import math
+# TODO: Make sure the GUI turns all values into floats so your type hinting is correct
+# TODO: Make sure in working with values that use pi that you limit significant figures displayed in the answer
 
 
-def circle(radius):
-    if isinstance(radius, str):
-        print('use a number!')
+def circle(radius: float) -> float:
+    """
+    Takes a value for a circle's radius and calculates area from that value
+    :param radius: float value for circle's radius
+    :return: The circle's area
+    """
+    if isinstance(radius, str) or isinstance(radius, bool):
+        print('(circle) use a number!')
         raise TypeError
 
     if radius <= 0:
-        print('radius cannot be less than one!!')
+        print('(circle) radius cannot be less than one!!')
         raise ValueError
 
     a_of_c = math.pi * radius ** 2
     return a_of_c
 
 
-def square(base):
-    if isinstance(base, str):
-        print('use a number!')
+def square(base: float) -> float:
+    """
+    Takes a value for a square's side and calculates area from that value
+    :param base: float value for square's side
+    :return: area of the square
+    """
+    if isinstance(base, str) or isinstance(base, bool):
+        print('(square) use a number!')
         raise TypeError
 
     if base <= 0:
-        print('side cannot be less than one!')
+        print('(square) side cannot be less than one!')
         raise ValueError
 
     a_of_s = base ** 2
     return a_of_s
 
 
-def rectangle(base, height):
-    if isinstance(base, str) or isinstance(height, str):
-        print('use a number!')
+def rectangle(base: float, height: float) -> float:
+    """
+    takes a rectangle's base and height as arguments and calculates the area from those values
+    :param base: value of the base of the rectangle
+    :param height: value of the height of the rectangle
+    :return: value of the area of the rectangle
+    """
+    if (isinstance(base, str) or isinstance(base, bool)) or (isinstance(height, str) or isinstance(height, bool)):
+        print('(rectangle) use a number!')
         raise TypeError
 
     if base <= 0 or height <= 0:
-        print('base or height cannot be less than one!')
+        print('(rectangle) base or height cannot be less than one!')
         raise ValueError
 
     a_of_r = base * height
     return a_of_r
 
 
-def triangle(base, height):
-    if isinstance(base, str) or isinstance(height, str):
-        print('use a number!')
+def triangle(base: float, height: float) -> float:
+    """
+    takes a triangle's base and height as arguments and calculates the area from those values
+    :param base: value of the base of the triangle
+    :param height: value of the height of the triangle
+    :return: value of the area of the rectangle
+    """
+    if (isinstance(base, str) or isinstance(base, bool)) or (isinstance(height, str) or isinstance(height, bool)):
+        print('(triangle) use a number!')
         raise TypeError
 
     if base <= 0 or height <= 0:
-        print('base or height cannot be less than one!')
+        print('(triangle) base or height cannot be less than one!')
         raise ValueError
 
     a_of_t = (1/2) * (base * height)
--- test_rashawn_thompson.py
+++ test_rashawn_thompson.py
@@ -9,6 +9,7 @@ class TestRashawn (unittest.TestCase):
         self.assertEqual(circle(5), math.pi * 25)
         self.assertEqual(circle(5.5), math.pi * 30.25)
         self.assertRaises(TypeError, circle, 'str')
+        self.assertRaises(TypeError, circle, True)
         self.assertRaises(ValueError, circle, 0)
         self.assertRaises(ValueError, circle, -1)
 
@@ -16,6 +17,7 @@ class TestRashawn (unittest.TestCase):
         self.assertEqual(square(5), 25)
         self.assertEqual(square(5.5), 30.25)
         self.assertRaises(TypeError, square, 'str')
+        self.assertRaises(TypeError, square, True)
         self.assertRaises(ValueError, square, 0)
         self.assertRaises(ValueError, square, -1)
 
@@ -29,6 +31,10 @@ class TestRashawn (unittest.TestCase):
         self.assertRaises(TypeError, rectangle, 'str', 10)
         self.assertRaises(TypeError, rectangle, 5, 'str2')
         self.assertRaises(TypeError, rectangle, 'str', 'str2')
+
+        self.assertRaises(TypeError, rectangle, True, 10)
+        self.assertRaises(TypeError, rectangle, 5, True)
+        self.assertRaises(TypeError, rectangle, True, False)
 
         self.assertRaises(ValueError, rectangle, -5, 10)
         self.assertRaises(ValueError, rectangle, 5, -10)
@@ -48,6 +54,10 @@ class TestRashawn (unittest.TestCase):
         self.assertRaises(TypeError, triangle, 'str', 10)
         self.assertRaises(TypeError, triangle, 5, 'str2')
         self.assertRaises(TypeError, triangle, 'str', 'str2')
+
+        self.assertRaises(TypeError, triangle, True, 10)
+        self.assertRaises(TypeError, triangle, 5, True)
+        self.assertRaises(TypeError, triangle, True, False)
 
         self.assertRaises(ValueError, triangle, -5, 10)
         self.assertRaises(ValueError, triangle, 5, -10)
